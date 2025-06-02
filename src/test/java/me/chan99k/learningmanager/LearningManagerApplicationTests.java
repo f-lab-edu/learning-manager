4d15c3e13c92cc204980ac4e5bb2433cf9cb9b6c
김찬규 <73059667+chan99k@users.noreply.github.com>
2025-06-02 16:39:32 +0900

    [Infra] 프로젝트 초기 구성 준비 (#5)

---
+++ src/test/java/me/chan99k/learningmanager/LearningManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package me.chan99k.learningmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
