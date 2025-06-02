63083532f421e41e32cad0b90f6bffd150ac16f6
chan99k <kjkj5868@gmail.com>
2025-06-02 11:01:12 +0900

    feat : java/spring 프로젝트 생성

---
+++ src/main/java/me/chan99k/learningmanager/LearningManagerApplication.java
@@ -0,0 +1,13 @@
+package me.chan99k.learningmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningManagerApplication.class, args);
+	}
+
+}
