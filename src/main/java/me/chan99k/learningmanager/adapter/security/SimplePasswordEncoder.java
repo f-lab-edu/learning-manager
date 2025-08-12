3832e3053ccfc42376c77c67105a37a7f12ae992
chan99k <kjkj5868@gmail.com>
2025-08-12 23:24:55 +0900

    feat: 간단한 비밀번호 인코더 및 랜덤 닉네임 생성기 추가
    - 비밀번호 인코더 인터페이스 구현체 SimplePasswordEncoder 추가
    - 랜덤 닉네임 생성기 RandomNicknameGenerator 추가

---
+++ src/main/java/me/chan99k/learningmanager/adapter/security/SimplePasswordEncoder.java
@@ -0,0 +1,19 @@
+package me.chan99k.learningmanager.adapter.security;
+
+import org.springframework.stereotype.Component;
+
+import me.chan99k.learningmanager.domain.member.PasswordEncoder;
+
+@Component
+public class SimplePasswordEncoder implements PasswordEncoder {
+	// TODO :: Spring security의 BCryptPasswordEncoder 로 변경 필요
+	@Override
+	public String encode(String rawString) {
+		return rawString.toUpperCase();
+	}
+
+	@Override
+	public boolean match(String rawString, String encoded) {
+		return this.encode(rawString).equals(encoded);
+	}
+}
