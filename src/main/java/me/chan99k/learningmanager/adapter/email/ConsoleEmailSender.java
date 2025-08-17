8f52a7eab5ea21e770db6c766fbefb13d21fcb7d
chan99k <kjkj5868@gmail.com>
2025-08-17 16:09:45 +0900

    fix : 로거를 사용하여 로그를 출력하도록 변경

--- src/main/java/me/chan99k/learningmanager/adapter/email/ConsoleEmailSender.java
+++ src/main/java/me/chan99k/learningmanager/adapter/email/ConsoleEmailSender.java
@@ -1,5 +1,7 @@
 package me.chan99k.learningmanager.adapter.email;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
@@ -7,14 +9,15 @@ import me.chan99k.learningmanager.domain.member.EmailSender;
 
 @Component
 public class ConsoleEmailSender implements EmailSender {
+
+	private static final Logger log = LoggerFactory.getLogger(ConsoleEmailSender.class);
+
 	@Override
 	@Async("emailTaskExecutor")
 	public void sendSignUpConfirmEmail(String email, String token) {
 		var activateURL = "http://localhost:9999/api/v1/members/activate?token=" + token;
-
-		System.out.println("====== 회원가입 인증 이메일 ======");
-		System.out.printf("[System] 회원가입 인증 이메일을 발송하였습니다. 수신자: %s , 활성화 링크: %s , 활성화 토큰: %s\n",
-			email, activateURL, token);
-		System.out.println("====== 회원가입 인증 이메일 ======");
+		log.info("====== 회원가입 인증 이메일 ======");
+		log.info("[System] 회원가입 인증 이메일을 발송하였습니다. 수신자: {}, 활성화 링크: {}", email, activateURL);
+		log.info("====== 회원가입 인증 이메일 ======");
 	}
 }
