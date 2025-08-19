7cea8e21c0b029487fd47c1e3bc4d69f107268ae
chan99k <kjkj5868@gmail.com>
2025-08-19 19:52:56 +0900

    feat: 로그인 요청 및 응답 처리 객체 추가
    - MemberLogin 인터페이스에 Request 및 Response 레코드 추가
    - 이메일 및 비밀번호 유효성 검증 어노테이션 추가

--- src/main/java/me/chan99k/learningmanager/application/member/provides/MemberLogin.java
+++ src/main/java/me/chan99k/learningmanager/application/member/provides/MemberLogin.java
@@ -1,8 +1,25 @@
 package me.chan99k.learningmanager.application.member.provides;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
 /**
  * [P0] 사용자 로그인
  */
 public interface MemberLogin {
-	void login();
+	MemberLogin.Response login(MemberLogin.Request request);
+
+	record Request(
+		@Email
+		@NotBlank(message = "이메일 입력은 필수입니다")
+		String email,
+		@NotBlank(message = "비밀번호 입력은 필수입니다")
+		String password
+	) {
+	}
+
+	record Response(
+		String accessToken
+	) {
+	}
 }
