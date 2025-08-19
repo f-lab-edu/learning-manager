b2dee4a8391638c3794ab8d0fe152be773eaa98e
chan99k <kjkj5868@gmail.com>
2025-08-19 22:56:39 +0900

    feat: 회원 프로필 조회 인터페이스 추가
    - 회원 ID 또는 닉네임을 통해 회원 프로필 조회 기능 제공
    - 요청 및 응답을 위한 DTO 포함

---
+++ src/main/java/me/chan99k/learningmanager/application/member/provides/MemberProfileRetrieval.java
@@ -0,0 +1,23 @@
+package me.chan99k.learningmanager.application.member.provides;
+
+import jakarta.validation.constraints.NotBlank;
+
+public interface MemberProfileRetrieval {
+
+	MemberProfileRetrieval.Response getProfile(Long memberId);
+
+	MemberProfileRetrieval.Response getPublicProfile(String nickname);
+
+	record Request(
+		@NotBlank(message = "닉네임 입력값은 필수입니다")
+		String nickname
+	) {
+	}
+
+	// TODO :: 공개 API 와 회원용 API에서 제공하는 프로필 정보가 많이 달라지게 되면 DTO를 분리해야 함
+	record Response(
+		String profileImageUrl,
+		String selfIntroduction
+	) {
+	}
+}
