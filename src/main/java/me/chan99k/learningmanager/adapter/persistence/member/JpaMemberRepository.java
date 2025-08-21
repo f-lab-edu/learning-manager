5ec61689462ef25997c42ba6d1d2b3832f8479d8
김찬규 <73059667+chan99k@users.noreply.github.com>
2025-08-21 15:48:36 +0900

    주석 업데이트

--- src/main/java/me/chan99k/learningmanager/adapter/persistence/member/JpaMemberRepository.java
+++ src/main/java/me/chan99k/learningmanager/adapter/persistence/member/JpaMemberRepository.java
@@ -15,7 +15,7 @@ import me.chan99k.learningmanager.domain.member.Member;
 @Repository
 public interface JpaMemberRepository extends JpaRepository<Member, Long> {
 	// TODO :: 아래 내용 찾아 보기
-	// Hibernate는 SQL ResultSEt 에서 Member row를 중복해서 가져올 수 있다?
+	// Hibernate는 SQL ResultSEt 에서 Member row를 중복해서 가져올 수 있다? + EntityGraph
 	// DISTINCT 가 디비 쿼리로 나가지 않도록 하는게 좋지 않나
 	@Query("SELECT DISTINCT m FROM Member m JOIN FETCH m.accounts a WHERE a.email = :email")
 	@QueryHints({
