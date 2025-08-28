46c0e12cd72d95e90f0abed567fd3e73d65c6b77
chan99k <kjkj5868@gmail.com>
2025-08-28 18:34:55 +0900

    feat: 인증 컨텍스트 관련 예외 처리 및 비밀번호 변경 API 구현

---
+++ src/main/java/me/chan99k/learningmanager/adapter/web/member/MemberPasswordController.java
@@ -0,0 +1,34 @@
+package me.chan99k.learningmanager.adapter.web.member;
+
+import java.util.concurrent.CompletableFuture;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.validation.Valid;
+import me.chan99k.learningmanager.application.member.provides.AccountPasswordChange;
+
+@RestController
+@RequestMapping("/api/v1/members")
+public class MemberPasswordController {
+	private final AccountPasswordChange passwordChangeService;
+
+	public MemberPasswordController(AccountPasswordChange passwordChangeService) {
+		this.passwordChangeService = passwordChangeService;
+	}
+
+	@PutMapping("/password")
+	public CompletableFuture<ResponseEntity<Void>> changePassword(
+		@Valid @RequestBody AccountPasswordChange.Request request
+	) {
+		return CompletableFuture.supplyAsync(() -> {
+			passwordChangeService.changePassword(request);
+
+			return ResponseEntity.status(HttpStatus.OK).build();
+		});
+	}
+}
