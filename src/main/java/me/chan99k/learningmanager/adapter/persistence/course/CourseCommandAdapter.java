afe992ee4715aebc764b05190710dec53641bb9a
chan99k <kjkj5868@gmail.com>
2025-08-31 15:25:27 +0900

    feat : CourseCommandAdapter 구현체 작성
    - CourseCommandRepository 인터페이스에 대한 어댑터
    - 내부적으로 JpaCourseRepository 를 사용하도록 정의

---
+++ src/main/java/me/chan99k/learningmanager/adapter/persistence/course/CourseCommandAdapter.java
@@ -0,0 +1,20 @@
+package me.chan99k.learningmanager.adapter.persistence.course;
+
+import org.springframework.stereotype.Repository;
+
+import me.chan99k.learningmanager.application.course.requires.CourseCommandRepository;
+import me.chan99k.learningmanager.domain.course.Course;
+
+@Repository
+public class CourseCommandAdapter implements CourseCommandRepository {
+	private final JpaCourseRepository jpaCourseRepository;
+
+	public CourseCommandAdapter(JpaCourseRepository jpaCourseRepository) {
+		this.jpaCourseRepository = jpaCourseRepository;
+	}
+
+	@Override
+	public Course create(Course course) {
+		return jpaCourseRepository.save(course);
+	}
+}
