40277bfd4eeda97bf02e0db542fabf10784e4138
chan99k <kjkj5868@gmail.com>
2025-09-11 22:48:06 +0900

    AttendanceMongoRepository 에 findBySessionIdAndMemberId() 추가

--- src/main/java/me/chan99k/learningmanager/adapter/persistence/attendance/AttendanceMongoRepository.java
+++ src/main/java/me/chan99k/learningmanager/adapter/persistence/attendance/AttendanceMongoRepository.java
@@ -1,7 +1,10 @@
 package me.chan99k.learningmanager.adapter.persistence.attendance;
 
+import java.util.Optional;
+
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface AttendanceMongoRepository extends MongoRepository<AttendanceDocument, ObjectId> {
+	Optional<AttendanceDocument> findBySessionIdAndMemberId(Long sessionId, Long memberId);
 }
