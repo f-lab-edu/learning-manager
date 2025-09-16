ea4b759fe2294fe95a39b9e7e4021f9bb201ac19
chan99k <kjkj5868@gmail.com>
2025-09-16 12:55:49 +0900

    수동 빈 구성으로 변경하기 위해 어노테이션 삭제

--- src/main/java/me/chan99k/learningmanager/common/config/MongoConfig.java
+++ src/main/java/me/chan99k/learningmanager/common/config/MongoConfig.java
@@ -4,11 +4,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
-import io.mongock.runner.springboot.EnableMongock;
-
 @Configuration
 @EnableMongoAuditing
-@EnableMongock
 @EnableMongoRepositories(basePackages = "me.chan99k.learningmanager.adapter.persistence.attendance")
 public class MongoConfig {
 }
