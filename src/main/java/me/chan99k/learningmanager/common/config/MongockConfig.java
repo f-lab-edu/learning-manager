7c5d48e7c03c795d5ca7676b35a1180e22e4ac3b
chan99k <kjkj5868@gmail.com>
2025-09-16 15:24:16 +0900

    AttendanceCommandAdapter 및 관련 테스트 케이스 추가
    - Attendance 저장 로직에 대한 다양한 성공/실패 시나리오 테스트 케이스 작성
    - AttendanceReservation 정리 로직에 대한 테스트 케이스 추가
    
    - 테스트 환경에서 사용하는 MongoDB 연결 설정(application.yml) 업데이트
    - 테스트 환경에서 Mongock 비활성화하도록 @Profile 애너테이션 적용

--- src/main/java/me/chan99k/learningmanager/common/config/MongockConfig.java
+++ src/main/java/me/chan99k/learningmanager/common/config/MongockConfig.java
@@ -3,12 +3,14 @@ package me.chan99k.learningmanager.common.config;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.mongodb.core.MongoTemplate;
 
 import io.mongock.runner.springboot.MongockSpringboot;
 import io.mongock.runner.springboot.base.MongockApplicationRunner;
 
 @Configuration
+@Profile("!test") // 테스트 환경에서는 Mongock 비활성화 -> 추후 통합 테스트 작성시 변경 필요
 public class MongockConfig {
 
 	@Bean
