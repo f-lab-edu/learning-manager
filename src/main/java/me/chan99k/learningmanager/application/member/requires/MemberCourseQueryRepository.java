ea5e617dd6e6ddf7376af4135b019906c98d1220
chan99k <kjkj5868@gmail.com>
2025-09-18 21:24:26 +0900

    CourseParticipationInfo DTO 독립화 및 DTO 프로젝션 인터페이스 추가

--- src/main/java/me/chan99k/learningmanager/application/member/requires/MemberCourseQueryRepository.java
+++ src/main/java/me/chan99k/learningmanager/application/member/requires/MemberCourseQueryRepository.java
@@ -2,6 +2,7 @@ package me.chan99k.learningmanager.application.member.requires;
 
 import java.util.List;
 
+import me.chan99k.learningmanager.application.member.CourseParticipationInfo;
 import me.chan99k.learningmanager.domain.course.Course;
 
 public interface MemberCourseQueryRepository {
@@ -9,5 +10,7 @@ public interface MemberCourseQueryRepository {
 	List<Course> findManagedCoursesByMemberId(Long memberId);
 
 	List<Course> findParticipatingCoursesByMemberId(Long memberId);
+
+	List<CourseParticipationInfo> findParticipatingCoursesWithRoleByMemberId(Long memberId);
 
 }
